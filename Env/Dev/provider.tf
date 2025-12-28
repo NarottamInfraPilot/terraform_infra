9883fe13a1af000e26fd4d1d573bee02891f3386
Narottam <133843446+NarottamInfraPilot@users.noreply.github.com>
2025-12-28 20:15:40 +0530

    Configure Azure backend for Terraform
    Added backend configuration for Azure storage.

--- Env/Dev/provider.tf
+++ Env/Dev/provider.tf
@@ -1,14 +1,19 @@
 terraform {
   required_providers {
     azurerm = {
-        source = "hashicorp/azurerm"
-        version = "~>3.0"
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
     }
   }
+ 
 backend "azurerm" {
+    resource_group_name  = "shruti-rg"
+    storage_account_name = "shrutiaccount2608"
+    container_name       = "mycontainer"
+    key                  = "shruti.tfstate"
+ 
+  }
 }
-}
-
 provider "azurerm" {
   features {}
 }
